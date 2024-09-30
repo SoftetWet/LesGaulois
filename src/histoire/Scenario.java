ea30bff077c74debe9f8f4bb13d4a456b2e5da80
Nino Devalette amiache <dvn4787a@ensfsi.onmicrosoft.com>
2024-09-30 16:26:11 +0200

    le village gaulois

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -7,7 +7,6 @@ import personnages.Romain;
 public class Scenario {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Gaulois asterix = new Gaulois("Asterix", 8);
 		Gaulois obelix = new Gaulois("Obélix", 25);
 		Romain minus = new Romain("Minus", 6);
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,12 +2,52 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private Chef chef;
+	private int nbVillageois = 0;
+	private Gaulois[] villageois;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum];
+	}
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 
 	public String getNom() {
 		return nom;
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois ++;
+	}
+
+	public Gaulois trouverHabitant(int numéro) {
+		return villageois[numéro];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
+		for (int i = 0; i < nbVillageois; i++){
+			System.out.println("- " + villageois[i].getNom() + "\n");
+		}
+	}
+
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		On obtient une erreur car le nb maximum étatnt 30 alors trouverhabitant(30) sors du tableau de villageois.
+		Chef abraracourcix = new Chef("Abraracourcis",6,village);
+		village.setChef(abraracourcix);
+		Gaulois asterix = new Gaulois("Asterix",8);
+		village.ajouterHabitant(asterix);
+//		Gaulois gaulois = village.trouverHabitant(1);
+//		System.out.println(gaulois);
+//		Ici les instructions renvoit null car astérix est a la case 0 du tableau car le chef n'est pas considérer un villageois et donc n'est pas dans le tableau
+		Gaulois obelix = new Gaulois("Obelix",25);
+		village.ajouterHabitant(obelix);
+		village.afficherVillageois();
 	}
 }
